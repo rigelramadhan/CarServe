28f5aa48c7b1bbf1e935bb38231dd2f03dbf4297
Rigel Ramadhan <rigelramadhan@gmail.com>
2024-07-12 09:39:12 +0700

    📝 Setup add car feature

---
+++ feature/vehicle/src/main/java/one/reevdev/carserve/vehicle/screen/AddVehicleScreen.kt
@@ -0,0 +1,9 @@
+package one.reevdev.carserve.vehicle.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AddVehicleScreen(modifier: Modifier = Modifier) {
+
+}
